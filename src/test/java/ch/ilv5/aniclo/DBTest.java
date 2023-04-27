110e0280a6303e01173a4ec478bf81e1f24f5bc2
fynnhaeny <fynn.haeny@bbzbl-it.ch>
2023-04-27 10:49:27 +0200

    add tests

---
+++ src/test/java/ch/ilv5/aniclo/DBTest.java
@@ -0,0 +1,25 @@
+package ch.ilv5.aniclo;
+
+import ch.ilv5.aniclo.model.Places;
+import ch.ilv5.aniclo.repository.PlacesRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback()
+public class DBTest {
+
+    @Autowired
+    private PlacesRepository placesRepository;
+
+    @Test
+    void insertPlace() {
+        Places places = this.placesRepository.save(new Places(1L, 12, "asdas", "sad", 1));
+        Assertions.assertNotNull(places.getId());
+    }
+}
---
+++ src/test/java/ch/ilv5/aniclo/RestControllerTests.java
@@ -0,0 +1,80 @@
+package ch.ilv5.aniclo;
+
+import ch.ilv5.aniclo.model.Places;
+import ch.ilv5.aniclo.repository.PlacesRepository;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+@AutoConfigureDataJpa
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class RestControllerTests {
+    @Autowired
+    private MockMvc api;
+
+    @Autowired
+    private PlacesRepository placesRepository;
+
+    @BeforeAll
+    void setup() {
+        this.placesRepository.save(new Places(1L, 213, "asd", "das", 2));
+        this.placesRepository.save(new Places(2L, 213, "asd", "asd", 213));
+    }
+
+    @Test
+    @Order(1)
+    void testGetVehicles() throws Exception {
+
+        String accessToken = obtainAccessToken();
+
+        api.perform(get("/places").header("Authorization", "Bearer " + accessToken)
+                        .with(csrf()))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("1")));
+    }
+
+    private String obtainAccessToken() {
+
+        RestTemplate rest = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        String body = "client_id=demoapp&" +
+                "grant_type=password&" +
+                "scope=openid profile roles offline_access&" +
+                "username=user&" +
+                "password=user";
+
+        HttpEntity<String> entity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> resp = rest.postForEntity("http://localhost:8080/realms/ILV/protocol/openid-connect/token", entity, String.class);
+
+        JacksonJsonParser jsonParser = new JacksonJsonParser();
+        return jsonParser.parseMap(resp.getBody()).get("access_token").toString();
+    }
+}
+
